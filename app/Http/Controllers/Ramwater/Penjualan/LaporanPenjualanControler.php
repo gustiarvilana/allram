860bf708087d9143cf33e29a8790fc7f3156c62c
guztiar.profile <guztiar.profile@gmail.com>
2023-11-02 10:42:52 +0700

    Laporan penjualan

--- app/Http/Controllers/Ramwater/Penjualan/LaporanPenjualanControler.php
+++ app/Http/Controllers/Ramwater/Penjualan/LaporanPenjualanControler.php
@@ -17,16 +17,14 @@ class LaporanPenjualanControler extends Controller
             ->select(
                 'c.nama as nama_sales',
                 'd.nama as nama_produk',
-
-                DB::raw('(SELECT SUM(jumlah) FROM ramwater_d_penjualan where kd_produk = a.kd_produk and nik = c.nik) as sum_jumlah'),
-                DB::raw('(SELECT SUM(total) FROM ramwater_d_penjualan_detail where id_penjualan = a.id) as sum_total'),
+                DB::raw('SUM(a.jumlah) as sum_jumlah'),
+                DB::raw('SUM(b.total) as sum_total'),
             )
             ->join('ramwater_d_penjualan_detail as b', 'a.id', 'b.id_penjualan')
             ->join('t_karyawan as c', 'a.nik', 'c.nik')
             ->join('t_master_produk as d', 'a.kd_produk', 'd.kd_produk')
             ->groupBy(
-                'a.id',
-                'a.nik',
+                // 'a.id',
                 'a.kd_produk',
                 'd.nama',
                 'c.nama',
